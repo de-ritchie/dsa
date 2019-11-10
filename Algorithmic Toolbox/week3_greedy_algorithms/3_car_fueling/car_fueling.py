ff330716a41e493433293b5a70276b9c977320dd
de-ritchie <chiraj.pepz@gmail.com>
2019-11-10 16:23:13 +0530

    Greedy Algo car fueling

--- Algorithmic Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ Algorithmic Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -4,7 +4,25 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code here
-    return -1
+
+    # print('----')
+    numRefill = currRefill = 0
+    stops = [0] + stops
+    n = len(stops)
+    stops.append(distance)
+    while currRefill < n :
+        lastRefill = currRefill
+        # print('----OUTSIDE')
+        while currRefill < n and ((stops[currRefill + 1] - stops[lastRefill]) <= tank) :
+            # print('----INSIDE')
+            currRefill += 1
+        if currRefill == lastRefill :
+            return -1
+        if currRefill < n :
+            numRefill += 1
+    return numRefill
+
+# print(compute_min_refills(950, 400, [200, 375, 550, 750]))
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
